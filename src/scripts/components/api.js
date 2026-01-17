0d6556156c11cedc9220c7833d98941a4233519e
Михаил Захаров <z27misha@gmail.com>
2026-01-17 21:51:07 +0500

    Update api.js

--- src/scripts/components/api.js
+++ src/scripts/components/api.js
@@ -1,7 +1,7 @@
 export const config = {
-  baseUrl: "https://mesto.nomoreparties.co/v1/apf-cohort-202",
+  baseUrl: "https://mesto.nomoreparties.co/v1/",
   headers: {
-    authorization: "78ae4154-0004-4faf-9ed2-fb4574eb9069",
+    authorization: "",
     "Content-Type": "application/json",
   },
 };
