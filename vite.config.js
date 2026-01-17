60dcead0ad04c2ff1bb2d7b0226de49202fae544
Михаил Захаров <z27misha@gmail.com>
2026-01-18 01:42:16 +0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite';  // defineConfig - функция помошник вызванная из vite
-
+import { defineConfig } from 'vite';
 
 export default defineConfig({
   server: {
-    open: true
-  }
+    open: true,
+  },
+  base: './',
 });
